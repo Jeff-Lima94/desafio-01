d114ad0b07cd1a71645da194135a6236277adc2b
Jeff-Lima94 <jeffersonlima10.jls@gmail.com>
2022-07-22 11:26:53 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome ="Jefferson"
+let age = 28
+let year = 2022
+let birth = (age-year)
+console.log (birth)
+let mensagem = "Estou mechendo no console.log"
+console.log (mensagem)
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
